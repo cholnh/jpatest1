b608c6d7fa81c5696c93ab2f61104a430b98f338
Nzzi <cholnh1@naver.com>
2021-08-01 14:51:39 +0900

    Initial commit

---
+++ src/main/java/kr/nzzi/test/jpatest1/configuration/DataSourceConfiguration.java
@@ -0,0 +1,23 @@
+package kr.nzzi.test.jpatest1.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean(name = "dataSource")
+    @Primary
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
